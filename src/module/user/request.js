3fecc8a8a93f38c38e3c9103384ef9c304bda22e
prioarief <prioariefgunawangunawan@gmail.com>
2021-11-24 13:36:04 +0700

    add email check on register

--- src/module/user/request.js
+++ src/module/user/request.js
@@ -1,25 +1,30 @@
-const Joi = require('joi');
-const { hash } = require('../../utilities/bcrypt');
-const wrapper = require('../../utilities/wrapper');
-const controller = require("./controller")
-
+const Joi = require("joi");
+const { hash } = require("../../utilities/bcrypt");
+const wrapper = require("../../utilities/wrapper");
+const controller = require("./controller");
+const { User } = require("../../db/models");
 
 const register = async (req, res) => {
-	try {
-        const schema = Joi.object({
-            name: Joi.string().required(),
-            password: Joi.string().required(),
-            email: Joi.string().email().required(),
-        });
-    
-        const { error, value } = schema.validate(req.body);
-        if (error) return wrapper(res, false, null, error.message, 400);
-        value.password = hash(value.password)
-        await controller.register(value)
-        return wrapper(res, true, value, null, 200)
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    const schema = Joi.object({
+      name: Joi.string().required(),
+      password: Joi.string().required(),
+      email: Joi.string().email().required(),
+    });
+
+    const { error, value } = schema.validate(req.body);
+    if (error) return wrapper(res, false, null, error.message, 400);
+    const emailIsExist = await User.findOne({
+      raw: true,
+      where: { email: value.email },
+    });
+    if (emailIsExist) return wrapper(res, false, "Email Already Exist", null, 400);
+    value.password = hash(value.password);
+    await controller.register(value);
+    return wrapper(res, true, "Register Success", null, 200);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = { register };
