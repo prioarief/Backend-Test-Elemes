24d5c7cc8e4a249141c1ec1758cf302c7b9df22b
prioarief <prioariefgunawangunawan@gmail.com>
2021-11-25 11:21:14 +0700

    create get report statistic endpoint where is delete = 0

--- src/module/report/controller.js
+++ src/module/report/controller.js
@@ -2,12 +2,12 @@ const { Op } = require("sequelize");
 const { User, Course } = require("../../db/models");
 
 const statistic = async () => {
-  const countUser = await User.count();
-  const countCourse = await Course.count();
+  const countUser = await User.count({ where: { is_delete: 0 } });
+  const countCourse = await Course.count({ where: { is_delete: 0 } });
   const countFreeCourse = await Course.count({
-    where: { price: { [Op.eq]: 0 } },
+    where: { price: { [Op.eq]: 0 }, is_delete: 0 },
   });
   return { countCourse, countFreeCourse, countUser };
 };
 
-module.exports = {statistic};
+module.exports = { statistic };
